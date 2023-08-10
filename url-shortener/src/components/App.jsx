47b78c85fabf17387131d9279cf543a0f1e3f16a
Deji Obasan <dejiobasan@yahoo.com>
2023-08-10 20:35:45 +0100

    >

--- url-shortener/src/components/App.jsx
+++ url-shortener/src/components/App.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../App.css"
+import "../App.css";
 import Form from "./form";
 
 function App() {
   return (
-    <div className="App">
-      <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path="/" Component={Form} />
-            <Route path="/app" Component={Form} />
-          </Switch>
+    <Router>
+      <div className="App">
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Switch>
+              <Route exact path="/" Component={Form} />
+              <Route path="/app" Component={Form} />
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
