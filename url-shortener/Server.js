dc0fb6534a4acb4e0d41e93c03d2c6a37b80cb8c
Deji Obasan <dejiobasan@yahoo.com>
2023-08-24 14:46:20 +0100

    added a proxy server

---
+++ url-shortener/Server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+// const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = 8000;
+
+app.get("/", (req, res) => {
+  const firebaseConfig = {
+    "apiKey": process.env.REACT_APP_API_KEY,
+    "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
+    "databaseURL": process.env.REACT_APP_DATABASE_URL,
+    "projectId": process.env.REACT_APP_PROJECT_ID,
+    "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
+    "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID,
+    "appId": process.env.REACT_APP_APP_ID,
+    "measurementId": process.env.REACT_APP_MEASUREMENT_ID
+  };
+  res.json(firebaseConfig);
+});
+
+app.listen(port, function () {
+  console.log("server started at port 8000.");
+});
--- url-shortener/src/index.js
+++ url-shortener/src/index.js
@@ -1,22 +1,28 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import axios from "axios";
 
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
-};
-
-
-initializeApp(firebaseConfig);
+axios
+  .get("http://localhost:8000/")
+  .then((response) => {
+    const firebaseConfig = {
+      apiKey: response.data.apiKey,
+      authDomain: response.data.authDomain,
+      databaseURL: response.data.databaseURL,
+      projectId: response.data.projectId,
+      storageBucket: response.data.storageBucket,
+      messagingSenderId: response.data.messagingSenderId,
+      appId: response.data.appId,
+      measurementId: response.data.measurementId,
+    };
+    initializeApp(firebaseConfig);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
